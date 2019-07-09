31fae3aff61943a6dbef7d31fa2373057af2ac29
Timothy 2 <timothy2@Timothys-MacBook-Air-2.local>
2019-07-09 10:20:09 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component }from 'react';
 import './App.css';
+import {
+  Route,
+  Link,
+  Switch,
+  Redriect,
+  BrowserRouter as Router
+} from 'react-router-dom';
 
-function App() {
+
+function Login() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>You need to login to see this</h2> 
+      <button>Login with Google</button>
     </div>
-  );
+  )
 }
+function Dashboard() {
+  return (
+    <div>
+      <h2>Welcome to Your Dashboard</h2> 
+    </div>
+  )
+}
+function home() {
+  return (
+    <div>
+      <h1>Welcome to React Firebase Todos</h1> 
+    </div>
+  )
+}
+
+
+class App extends Component {
+  render () {
+    return (
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/" component={home} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
 
 export default App;
